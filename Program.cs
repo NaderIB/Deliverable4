80024072d7e2989e33dcb72aba6bd080bc76388b
nader <nader@LAPTOP-6BSTED93>
2022-09-16 18:36:10 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Deliverable4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibonacci = new int[25];
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            for (int i = 2; i < 25; i++)
+            {
+                fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+
+                Console.WriteLine(fibonacci[i]);
+            }
+
+            
+
+        }
+    }
+}
